ecd7df705608b06f6d8665d8f724663d6f2ce2a2
Randy Fay <randy@randyfay.com>
2013-01-13 16:35:30 -0700

    Fix unclosed span in template that broke ie8

--- sites/all/modules/dev/wsmap/templates/wsmap-view.tpl.php
+++ sites/all/modules/dev/wsmap/templates/wsmap-view.tpl.php
@@ -6,16 +6,12 @@
 
 <a id="collapse_map" href="#"><?php print t('Collapse Map'); ?></a>
 <noscript>
-    <strong>
       <?php print t("This page isn't going to be much use to you until you turn on javascript in your browser. We apologize."); ?>
-    </strong>
 </noscript>
 
 <div id='mapframe' style='width:100%;position:relative;clear:both;'>
     <div id='mapholder'>
-        <div id='wsmap_map'>
-        </div>
-
+        <div id='wsmap_map'></div>
     </div>
 </div>
-<div id="wsmap-bottom"><span id="wsmap-load-status"></span><span class="wsmap-credits"><?php print t('Geolocation courtesy of Google and Geonames.org'); ?></div>
+<div id="wsmap-bottom"><span id="wsmap-load-status"></span><span class="wsmap-credits"><?php print t('Geolocation courtesy of Google and Geonames.org'); ?></span></div>
